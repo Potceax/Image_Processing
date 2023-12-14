2114107ca42fbffbbf6b7f5719859b19862a8a13
Karol Warda <karx132@gmail.com>
2023-12-14 16:22:15 +0100

    Mesure merge algorithm time, add file handle

--- Cpp/Test/Merge/Merge/Merge/Main.cpp
+++ Cpp/Test/Merge/Merge/Merge/Main.cpp
@@ -1,6 +1,23 @@
-void Merge(
-    const std::string& photoPath1, const std::string& photoPath2,
-    const std::string& outputPath, cv::Mat& output, float alpha = 0.5)
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <filesystem>
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+namespace fs = std::filesystem;
+
+/**
+  * Function to merge two photos together
+  * @param: string photoPathX -> path to original photos
+  * @param: float alpha -> alpha value of the top photo after merge
+  *
+  * @return: cv::Mat -> merged photo data
+*/
+static cv::Mat Merge(
+    const std::string& photoPath1, const std::string& photoPath2, float alpha = 0.5)
 {
     // Load the images
     cv::Mat image1 = cv::imread(photoPath1);
@@ -9,7 +26,7 @@ void Merge(
     if (image1.empty() || image2.empty())
     {
         std::cout << "Could not open or find the images!" << std::endl;
-        return;
+        return cv::Mat();
     }
 
     // Resize image2 to match image1
@@ -21,20 +38,87 @@ void Merge(
     // Transparency for image1
     float beta = (1.0 - alpha);  // Transparency for image2
 
+    // final result of merge
+    cv::Mat output;
+
     // Blend the images
     addWeighted(image1, alpha, image2, beta, 0.0, output);
 
-    // Save the blended image
-    imwrite(outputPath, output);
+    return output;
 }
+
+/**
+  * Function that mesures time for merging
+  * @param: input -> takes image path
+  * @param: output -> takes image path
+*/
+static double TestTime(const std::string& input1, const std::string& input2, int reps = 5)
+{
+    std::vector<double> time{};
+    double sum{};
+    for (int i{ 0 }; i < reps; ++i)
+    {
+        int64_t start = cv::getTickCount();
+        Merge(input1, input2);
+        int64_t end = cv::getTickCount();
+        time.push_back((end - start) / cv::getTickFrequency());
+
+        sum += time.back();
+        std::cout << "Time: " << time.back() << "s\n";
+    }
+
+    return (sum / reps);
+}
+
+/**
+  * Create new folder in the current one
+  * @param: string folderName -> name of the new folder
+  * @return: string           -> path to the new folder
+*/
+static std::string CreateNewFolder(const std::string& folderName)
+{
+    // Get the current working directory
+    fs::path currentDir = fs::current_path();
+
+    // Name of the new folder
+    std::string newFolderName = "merge";
+
+    // Construct the path to the new folder
+    fs::path newFolderPath = currentDir / newFolderName;
+
+    // Create directory
+    if (!fs::exists(newFolderPath))
+    {
+        fs::create_directory(newFolderPath);
+    }
+
+    std::string FolderPath{ newFolderPath.string() + "\\\\" };
+
+    return FolderPath;
+}
+
 
 int main()
 {
-    int main()
-    {
-        cv::Mat out;
-        Merge("Maklowicz.jpg", "mountain.png", "out.jpg", out);
+    std::string PhotoPath1{};
+    std::string PhotoPath2{};
+    std::string OutputPath{};
 
-        return 0;
+    // fill paths and create folder dedicated for output
+    std::ifstream file("Mergefile.txt"); // Replace with your file name
+    if (file.is_open())
+    {
+        // Read the first string from the file
+        file >> PhotoPath1 >> PhotoPath2 >> OutputPath;
+        file.close(); // Close the file after reading
     }
+    OutputPath = CreateNewFolder("Merge") + OutputPath;
+
+    // Time test for merge (5 reps)
+    std::cout << TestTime(PhotoPath1, PhotoPath2) << "s\n";
+
+    // Create output image
+    cv::imwrite(OutputPath, Merge(PhotoPath1, PhotoPath2));
+
+    return 0;
 }
--- Python/merge.py
+++ Python/merge.py
@@ -1,20 +1,61 @@
 from PIL import Image
+from typing import Callable
+import time
 
-def blend_images(image_path1, image_path2, output_path, alpha=0.5):
+N_REPS = 10
+PREP_FILE_PATH = "mergeFile.txt"
+
+def blend_images(image_path1, image_path2, alpha=0.5):
     # Open the images
     image1 = Image.open(image_path1)
     image2 = Image.open(image_path2)
 
     # Ensure images are the same size
     if image1.size != image2.size:
-        print("Images are not the same size. Resizing the second image to match the first.")
         image2 = image2.resize(image1.size)
 
     # Blend the images
     blended_image = Image.blend(image1, image2, alpha)
+    return blended_image
+    
 
-    # Save the new image
-    blended_image.save(output_path)
+def measure_time(function: Callable, *args, n_repetitions: int = N_REPS) -> float:
+    """Function mesuring argument "function" time
 
-# Example usage
-blend_images('chrupiaca.jpg', 'mam.jpg', 'out.jpg')
+    Args:
+        function (Callable): function being timed
+        n_repetitions (int): number of calls to function argument
+
+    Returns:
+        float: mesured time
+    """
+    times = []
+    for _ in range(n_repetitions):
+        t_0 = time.perf_counter()
+        function(*args)
+        t_1 = time.perf_counter()
+        times.append(t_1 - t_0)
+    mean_time = sum(times) / n_repetitions
+    return mean_time
+
+
+if __name__== "__main__":
+    # output in text variable should be formated this way:
+    # "image_path (string), output_path (string), radius (int)"
+    with open(PREP_FILE_PATH, "rt", encoding="utf8") as file:
+        text = file.read()
+
+    # dictionary containing initial values
+    text_list = [name.strip() for name in text.split(',')]
+    text_dir = { "image_path1": text_list[0], "image_path2": text_list[1], "output_path": text_list[2] }
+    
+
+    input_image_path1 = text_dir["image_path1"]
+    input_image_path2 = text_dir["image_path2"]
+    blend_output_path = text_dir["output_path"]
+
+    print(f'total time: {measure_time(blend_images, input_image_path1, input_image_path2)} s')
+
+    # Example usage
+    blended_image = blend_images(input_image_path1, input_image_path2)
+    blended_image.save(blend_output_path)
